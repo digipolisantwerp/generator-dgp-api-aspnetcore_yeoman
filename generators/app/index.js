06a652485d685756729babbd80fb6189f15506be
Steven Vanden Broeck <xytyx@telenet.be>
2016-03-31 09:30:32 +0200

    Version number.

--- generators/app/index.js
+++ generators/app/index.js
@@ -12,7 +12,7 @@ module.exports = yeoman.generators.Base.extend({
     var done = this.async();
 
     // greet the user
-    this.log(yosay('Welcome to the fantastic Yeoman ' + chalk.green('dgp-api-aspnetcore') + ' ' + chalk.blue('(1.0.1)') + ' generator!'));
+    this.log(yosay('Welcome to the fantastic Yeoman ' + chalk.green('dgp-api-aspnetcore') + ' ' + chalk.blue('(1.0.3)') + ' generator!'));
     
     // ask project parameters
     var prompts = [{
