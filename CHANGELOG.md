2c3b3be8117c1816546287960af21bb0a46426fa
Steven Vanden Broeck <xytyx@telenet.be>
2016-04-05 09:28:04 +0200

    Notify when a newer version is available.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -11,3 +11,39 @@
 ## 1.0.2
 
 - added Correlation Toolbox. 
+
+## 1.0.3
+
+- casing in file names
+
+## 1.0.4
+
+- added update notifier
+
+## 1.0.5
+
+- location of package.json
+
+## 1.0.6
+
+- no generation when newer version of the generator
+
+## 1.0.7
+
+- display current version
+
+## 1.0.8
+
+- test update notifier
+
+## 1.0.9
+
+- update interval to 5 minutes
+
+## 1.0.10
+
+- display welcome message first
+
+## 1.0.11
+
+- more tests update notifier
--- generators/app/index.js
+++ generators/app/index.js
@@ -5,6 +5,8 @@ var yosay = require('yosay');
 var del = require('del');
 var nd = require('node-dir');
 var Guid = require('guid');
+var updateNotifier = require('update-notifier');
+var pkg = require('./../../package.json');
 
 module.exports = yeoman.generators.Base.extend({
   
@@ -12,7 +14,14 @@ module.exports = yeoman.generators.Base.extend({
     var done = this.async();
 
     // greet the user
-    this.log(yosay('Welcome to the fantastic Yeoman ' + chalk.green('dgp-api-aspnetcore') + ' ' + chalk.blue('(1.0.3)') + ' generator!'));
+    this.log(yosay('Welcome to the fantastic Yeoman ' + chalk.green('dgp-api-aspnetcore') + ' ' + chalk.blue('(1.0.11)') + ' generator!'));
+    
+    var notifier = updateNotifier({
+        pkg,
+        updateCheckInterval: 1000 * 60 * 5      // check every 5 minutes. 
+    });
+    notifier.notify();
+    if (notifier.update != undefined) return;
     
     // ask project parameters
     var prompts = [{
