0469df854c8e502c424dc526d6cc0422a1c85782
Jonah Jordan <Jonah.Jordan@digipolis.be>
2020-07-06 11:31:21 +0200

    FEAT: capture errors on start and exception mapper

--- generators/app/templates/src/StarterKit/Program.cs
+++ generators/app/templates/src/StarterKit/Program.cs
@@ -77,6 +77,7 @@ namespace StarterKit
             logging.AddConsole();
             logging.AddDebug();
           })
+          .CaptureStartupErrors(true)
           .UseConfiguration(hostingConfig)
           .UseUrls(serverUrls);
     }
--- generators/app/templates/src/StarterKit/Startup/ApiExceptionMapper.cs
+++ generators/app/templates/src/StarterKit/Startup/ApiExceptionMapper.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Net;
+using AutoMapper;
+using Digipolis.Correlation;
 using Digipolis.Errors;
 using Digipolis.Errors.Exceptions;
 using Microsoft.AspNetCore.Hosting;
@@ -12,16 +14,18 @@ namespace StarterKit.Startup
 {
   public class ApiExceptionMapper : ExceptionMapper
   {
-    public ApiExceptionMapper(ILogger<ApiExceptionMapper> logger, IApplicationLogger appLogger, IWebHostEnvironment environment) : base()
+    public ApiExceptionMapper(ILogger<ApiExceptionMapper> logger, IApplicationLogger appLogger, IWebHostEnvironment environment, ICorrelationService correlationService) : base()
     {
       Logger = logger ?? throw new ArgumentException($"{GetType().Name}.Ctr parameter {nameof(logger)} cannot be null.");
       AppLogger = appLogger ?? throw new ArgumentException($"{GetType().Name}.Ctr parameter {nameof(appLogger)} cannot be null.");
       _environment = environment ?? throw new ArgumentException($"{GetType().Name}.Ctr parameter {nameof(environment)} cannot be null.");
+      _correlationService = correlationService ?? throw new ArgumentException($"{GetType().Name}.Ctr parameter {nameof(environment)} cannot be null.");
     }
 
     protected ILogger<ApiExceptionMapper> Logger { get; private set; }
     protected IApplicationLogger AppLogger { get; private set; }
     private readonly IWebHostEnvironment _environment;
+    private readonly ICorrelationService _correlationService;
 
     protected override void Configure()
     {
@@ -33,22 +37,57 @@ namespace StarterKit.Startup
       {
         CreateValidationMap(error, new ValidationException(exception: exception));
       });
+      CreateMap<ForbiddenException>((error, exception) =>
+      {
+        error.Status = (int)HttpStatusCode.Forbidden;
+        error.Title = "Forbidden";
+        error.Identifier = GetIdentifier();
+        error.ExtraInfo = exception.Messages;
+      });
+      CreateMap<AutoMapperMappingException>((error, exception) =>
+      {
+        if (exception.InnerException?.Message == null)
+          CreateDefaultMap(error, exception);
+        else
+        {
+          if (exception.InnerException is System.ComponentModel.DataAnnotations.ValidationException
+              || exception.InnerException is ValidationException)
+            CreateValidationMap(error, new ValidationException(exception: exception.InnerException));
+          else if (exception.InnerException is NotFoundException notFoundException)
+            CreateNotFoundMap(error, notFoundException);
+          else if (exception.InnerException is UnauthorizedException innerException)
+            CreateUnauthorizedMap(error, innerException);
+          else if (exception.InnerException is ForbiddenException)
+            CreateMap<ForbiddenException>((e, ex) =>
+            {
+              error.Status = (int)HttpStatusCode.Forbidden;
+              error.Title = "Forbidden";
+              error.Identifier = GetIdentifier();
+              error.ExtraInfo = ((ForbiddenException)exception.InnerException).Messages;
+            });
+          else
+            CreateDefaultMap(error, exception.InnerException);
+        }
+      });
     }
 
     protected override void CreateNotFoundMap(Error error, NotFoundException exception)
     {
       base.CreateNotFoundMap(error, exception);
+      error.Identifier = GetIdentifier();
       Logger.LogWarning($"Not found: {exception.Message}", exception);
     }
 
     protected override void CreateUnauthorizedMap(Error error, UnauthorizedException exception)
     {
       base.CreateUnauthorizedMap(error, exception);
+      error.Identifier = GetIdentifier();
       Logger.LogWarning($"Access denied: {exception.Message}", exception);
     }
 
     protected override void CreateDefaultMap(Error error, Exception exception)
     {
+      error.Identifier = GetIdentifier();
       if (_environment.IsDevelopment())
       {
         error.Status = (int)HttpStatusCode.InternalServerError;
@@ -69,6 +108,8 @@ namespace StarterKit.Startup
     {
       error.ExtraInfo = exception.Messages;
       base.CreateValidationMap(error, exception);
+
+      error.Identifier = GetIdentifier();
       error.Title = exception.Message;
       Logger.LogWarning($"Validation error: {exception.GetExceptionMessages()}, {exception.ToString()}");
       AppLogger.LogWarning($"Er is een validatiefout opgetreden. {exception.GetExceptionMessages()}");
@@ -77,9 +118,19 @@ namespace StarterKit.Startup
     private void AddInnerExceptions(Error error, Exception exception, int level = 0)
     {
       if (exception.InnerException == null) return;
+
       error.ExtraInfo[$"InnerException{level:00}"] = new[] { $"{exception.InnerException.GetType().Name}: {exception.InnerException.Message}" };
       AddInnerExceptions(error, exception.InnerException, level++);
     }
-  }
 
+    private Guid GetIdentifier()
+    {
+      if (!Guid.TryParse(_correlationService.GetContext().Id, out var identifier))
+      {
+        identifier = Guid.NewGuid();
+      }
+
+      return identifier;
+    }
+  }
 }
