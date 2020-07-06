558081704953a62d63ca014236095f259a3e2338
Jonah Jordan <Jonah.Jordan@digipolis.be>
2020-07-06 11:05:34 +0200

    FEAT: added some const config keys

---
+++ generators/app/templates/src/StarterKit/Shared/Constants/AppSettingsConfigKey.cs
@@ -0,0 +1,14 @@
+namespace StarterKit.Shared.Constants
+{
+  public class AppSettingsConfigKey
+  {
+    public const string ServerUrls = "SERVER_URLS";
+    public const string LocalServerUrls = "server.urls";
+    public const string AppName = "APPSETTINGS_APPNAME";
+    public const string ApplicationId = "APPSETTINGS_APPLICATIONID";
+    public const string DataDirectory = "APPSETTINGS_DATADIRECTORY";
+    public const string TempDirectory = "APPSETTINGS_TEMPDIRECTORY";
+    public const string LogExceptions = "APPSETTINGS_LOGEXCEPTIONS";
+    public const string DisableGlobalErrorHandling = "APPSETTINGS_DISABLEGLOBALERRORHANDLING";
+  }
+}
